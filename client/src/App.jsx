85ff3b8a9aca7b5d0dd5311fb0dbb6fb7ecf6b69
sunminn-byte <sunminn0627@gmail.com>
2025-12-08 17:58:27 +0900

    251208 [client] PWA & service worker 적용 & 구독 등록 처리

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { Outlet } from 'react-router-dom';
 import './App.css';
 import Header from './components/common/Header.jsx';
 import ProtectedRouter from './routes/ProtectedRouter.jsx';
+import NotificationInfo from './components/subscriptions/NotificationInfo.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <ProtectedRouter />
+      <NotificationInfo />
     </>
   )
 }
